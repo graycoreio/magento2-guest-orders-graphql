286be84135440a2424fad227279af0bc9a41146e
Peter Lauck <griest024@gmail.com>
2020-10-20 00:15:03 -0400

    test: add smoke test

---
+++ Test/Integration/SmokeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Copyright © Graycore, LLC. All rights reserved.
+ * See LICENSE.md for details.
+ */
+
+namespace Graycore\GuestOrdersGraphQl\Test\Integration;
+
+/**
+ * A simple smoke test
+ * @author    Graycore <peter.lauck@graycore.io>
+ * @copyright Graycore, LLC (https://www.graycore.io/)
+ * @license   MIT https://github.com/graycoreio/magento2-guest-orders-graphql/license
+ * @link      https://github.com/graycoreio/magento2-guest-orders-graphql
+ */
+class SmokeTest extends \PHPUnit\Framework\TestCase
+{
+    public function testItShouldPass()
+    {
+        $this->assertEquals(true, true);
+    }
+}
